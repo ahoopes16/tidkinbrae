799ae4068a6e9e352f48ab90f7d9ab3da85efa5c
Kevin Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-08 00:47:00 -0700

    Blank Pages (#7)
    * Create basic About Me page
    
    * Create basic Ambitions page
    
    * Create basic Education page
    
    * Create basic Experience page
    
    * Create basic Projects page
    
    * Remove navbar animation because it looks bad on re-renders

---
+++ src/pages/ambitions.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '../components/mainlayout'
+
+export default function Ambitions() {
+  return (
+    <MainLayout>
+      <h1 className='fade-in'>Ambitions & Goals</h1>
+    </MainLayout>
+  )
+}
---
+++ src/pages/projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '../components/mainlayout'
+
+export default function Projects() {
+  return (
+    <MainLayout>
+      <h1 className='fade-in'>Personal Projects</h1>
+    </MainLayout>
+  )
+}
