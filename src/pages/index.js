81460ec5c70bcc133387359bc1bb2a1469861423
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-07 23:28:45 -0700

    Create a Navbar that will get wrapped around all of the pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React from 'react'
+import MainLayout from '../components/mainlayout'
 
 export default function Home() {
-  return <h1 className='fade-in'>Kevin (Alex) Hoopes</h1>
+  return (
+    <MainLayout>
+      <h1 className='fade-in'>Kevin (Alex) Hoopes</h1>
+    </MainLayout>
+  )
 }
