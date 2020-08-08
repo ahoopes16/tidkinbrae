dc474d5dee1c19d5d8625b274e2e2bce36b1da7b
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-08 00:40:42 -0700

    Create basic About Me page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '../components/mainlayout'
+
+export default function About() {
+  return (
+    <MainLayout>
+      <h1 className='fade-in'>About Me</h1>
+    </MainLayout>
+  )
+}
