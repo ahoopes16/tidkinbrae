3526a3bdbd2a5bc9911cd8b9e8683abc2f3910c5
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-08 00:41:17 -0700

    Create basic Experience page

---
+++ src/pages/experience.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainLayout from '../components/mainlayout'
+
+export default function Experience() {
+  return (
+    <MainLayout>
+      <h1 className='fade-in'>Experience</h1>
+    </MainLayout>
+  )
+}
