79f55aeaa66ba6f65fe2c32680538947399cda73
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-08 22:24:28 -0700

    Create Contact Area and Content Page Layout components

---
+++ src/components/contentpagelayout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContentPageLayout({ children }) {
+    return (
+        <div className='content-page'>
+            {children}
+        </div>
+    )
+}
