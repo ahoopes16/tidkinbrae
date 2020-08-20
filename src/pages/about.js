06f6984cbbf68beff300fba1a368c98a71e41bb8
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-19 23:01:23 -0700

    Change grid layout so the title sits where it's supposed to
    I got a lot of help understanding grids by playing
    Grid Garden (https://cssgridgarden.com/)

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,7 @@ import React from 'react'
 import MainLayout from '../components/mainlayout'
 
 export default function About() {
-  return (
-    <MainLayout>
-      <h1 className='fade-in'>About Me</h1>
-    </MainLayout>
-  )
+    return (
+        <MainLayout title='About Me' />
+    )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,9 +2,7 @@ import React from 'react'
 import MainLayout from '../components/mainlayout'
 
 export default function Projects() {
-  return (
-    <MainLayout>
-      <h1 className='fade-in'>Personal Projects</h1>
-    </MainLayout>
-  )
+    return (
+        <MainLayout title='Personal Projects' />
+    )
 }
