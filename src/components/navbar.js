1012db0bd32ae44d8e27cb20895904cb8fb9a0b0
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-20 21:41:24 -0700

    Add 'selected' class based on which tab is selected

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,25 +2,37 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 export default function Navbar({ selectedTab }) {
+    const getSelectedClass = sectionName => selectedTab === sectionName ? 'selected' : ''
+
     return (
         <div className='navbar'>
-            <Link to='/about'>About</Link>
+            <span className={getSelectedClass('about')}>
+                <Link to='/about'>About</Link>
+            </span>
 
             <span className='navbar-divider'>\</span>
 
-            <Link to='/ambitions'>Ambitions</Link>
+            <span className={getSelectedClass('ambitions')}>
+                <Link to='/ambitions'>Ambitions</Link>
+            </span>
 
             <span className='navbar-divider'>\</span>
 
-            <Link to='/education'>Education</Link>
+            <span className={getSelectedClass('education')}>
+                <Link to='/education'>Education</Link>
+            </span>
 
             <span className='navbar-divider'>\</span>
 
-            <Link to='/experience'>Experience</Link>
+            <span className={getSelectedClass('experience')}>
+                <Link to='/experience'>Experience</Link>
+            </span>
 
             <span className='navbar-divider'>\</span>
 
-            <Link to='/projects'>Projects</Link>
+            <span className={getSelectedClass('projects')}>
+                <Link to='/projects'>Projects</Link>
+            </span>
         </div>
     )
 }
