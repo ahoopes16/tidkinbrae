d07f84211dcae47b0d80c17488ad83e3bbfb9ec9
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-20 23:42:33 -0700

    Add a placeholder image in lieu of my actual self portrait for now

--- src/components/contactarea.js
+++ src/components/contactarea.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ContactArea() {
   return (
     <div className='contact-area'>
+      <img className='self-portrait' src='https://i.pinimg.com/originals/0a/53/f6/0a53f615118c87ce9573f3d1f429e0eb.jpg'/>
     </div>
   )
 }
