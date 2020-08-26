f47c21e89a317cd1cc59a96c040fb310e96c3a3a
Kevin Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-25 23:08:24 -0700

    Replace Picture with Real Photo of Me (#28)
    * Replace photo of me with cropped version
    
    * Replace blobfish picture with new picture of me and Nikki

--- src/components/contact/profilepic.js
+++ src/components/contact/profilepic.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import ProfilePicPath from '../../assets/engaged.jpg'
 
 export default function ProfilePic() {
     return (
         <img
             className='self-portrait'
-            alt='My beautiful face'
-            src='https://i.pinimg.com/originals/0a/53/f6/0a53f615118c87ce9573f3d1f429e0eb.jpg'
+            alt='Me (left) and my beautiful fiancee (right)'
+            src={ProfilePicPath}
         />
     )
 }
