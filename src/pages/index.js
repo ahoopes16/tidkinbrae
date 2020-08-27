70988841e83be00d05e6566f801a4917342ce851
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-08-26 23:14:29 -0700

    Fill the home page with a basic introduction to the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,15 @@ import MainLayout from '../components/layouts/mainlayout'
 
 export default function Home() {
   return (
-    <MainLayout title='My Portfolio' />
+    <MainLayout title='My Portfolio'>
+      <h1 style={{ textAlign: 'center' }}>Welcome!</h1>
+
+      <p>Hello, and welcome to my portfolio site.</p>
+      <p>
+        Please feel free to have a look around and stay as long as you'd like.
+        There's plenty to learn about me; you can start by clicking on any of the links at the top of the page to get started.
+      </p>
+
+    </MainLayout>
   )
 }
